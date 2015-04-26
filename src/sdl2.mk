dcc1249bd89a14503b4d37c32e431f5bf53cf8ec
AmatCoder <amatcoder@gmail.com>
2015-04-26 06:08:20 +0200

    sdl2: disable features Mednaffe only needs joystick and events features

--- src/sdl2.mk
+++ src/sdl2.mk
@@ -19,8 +19,61 @@ endef
 define $(PKG)_BUILD
     cd '$(1)' && aclocal -I acinclude && autoconf && ./configure \
         $(MXE_CONFIGURE_OPTS) \
-        --enable-threads \
-        --enable-directx
+        --disable-threads \
+        --enable-directx \
+        --disable-audio \
+        --enable-video \
+        --disable-render \
+        --enable-events \
+        --enable-joystick \
+        --enable-haptic \
+        --disable-power \
+        --enable-filesystem \
+        --disable-timers \
+        --disable-file \
+        --disable-loadso \
+        --disable-cpuinfo \
+        --disable-oss \
+        --disable-alsa \
+        --disable-alsatest \
+        --disable-alsa-shared \
+        --disable-esd \
+        --disable-esdtest \
+        --disable-esd-shared \
+        --disable-pulseaudio \
+        --disable-pulseaudio-shared \
+        --disable-arts \
+        --disable-arts-shared \
+        --disable-nas \
+        --disable-nas-shared \
+        --disable-sndio \
+        --disable-sndio-shared \
+        --disable-diskaudio \
+        --disable-dummyaudio \
+        --disable-video-wayland \
+        --disable-video-wayland-qt-touch \
+        --disable-wayland-shared \
+        --disable-video-mir \
+        --disable-mir-shared \
+        --disable-video-x11 \
+        --disable-x11-shared \
+        --disable-video-x11-xcursor \
+        --disable-video-x11-xinerama \
+        --disable-video-x11-xinput \
+        --disable-video-x11-xrandr \
+        --disable-video-x11-scrnsaver \
+        --disable-video-x11-xshape \
+        --disable-video-x11-vm \
+        --disable-video-cocoa \
+        --disable-directfb-shared \
+        --disable-fusionsound-shared \
+        --disable-video-dummy \
+        --disable-video-opengl \
+        --disable-video-opengles \
+        --disable-libudev \
+        --disable-dbus \
+        --disable-input-tslib \
+        --disable-render-d3d
     $(SED) -i 's,defined(__MINGW64_VERSION_MAJOR),defined(__MINGW64_VERSION_MAJOR) \&\& defined(_WIN64),' '$(1)/include/SDL_cpuinfo.h'
     $(SED) -i 's,-XCClinker,,' '$(1)/sdl2.pc'
     $(SED) -i 's,-XCClinker,,' '$(1)/sdl2-config'
