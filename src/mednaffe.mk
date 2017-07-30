53ed4991b5c05e86f321d0887ca83966bbf11631
AmatCoder <amatcoder@gmail.com>
2017-07-30 11:57:44 +0200

    Update to 0.8.6

--- src/mednaffe.mk
+++ src/mednaffe.mk
@@ -3,8 +3,8 @@
 
 PKG             := mednaffe
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.8.4
-$(PKG)_CHECKSUM := 8ef89330d4b128d68b4f17f7ca760b6870d72878
+$(PKG)_VERSION  := 0.8.6
+$(PKG)_CHECKSUM := 529107f5f4209792e81dce9f8bdf337aa0bb0946
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION)-src.zip
 $(PKG)_URL      :=https://sites.google.com/site/amatcoder/mednaffe/downloads/$($(PKG)_FILE)?attredirects=0&d=1
@@ -17,5 +17,5 @@ endef
 
 define $(PKG)_BUILD
     cd '$(1)'/share/win && $(TARGET)-windres mednaffe.rc -O coff -o mednaffe.res && mv -f *.txt -t ../../../../
-    cd '$(1)'/src && $(TARGET)-gcc -Wl,--export-all-symbols -s -O2 -std=c99 -Wall -DGTK2_ENABLED -DSTATIC_ENABLED -mwindows -mms-bitfields -o mednaffe.exe mednaffe.c active.c command.c gui.c list.c toggles.c prefs.c about.c input.c log.c  joystick_win.c md5.c resource.c ../share/win/mednaffe.res `i686-w64-mingw32.static-pkg-config --libs --cflags sdl2 gtk+-2.0 gmodule-export-2.0` && mv -f mednaffe.exe ../../../mednaffe.exe
+    cd '$(1)'/src && $(TARGET)-gcc -Wl,--export-all-symbols -O2 -s -std=c99 -Wall -DGTK2_ENABLED -DSTATIC_ENABLED -mwindows -mms-bitfields -o mednaffe.exe mednaffe.c active.c command.c gui.c list.c toggles.c prefs.c about.c input.c log.c  joystick_win.c md5.c resource.c ../share/win/mednaffe.res `i686-w64-mingw32.static-pkg-config --libs --cflags sdl2 gtk+-2.0 gmodule-export-2.0` && mv -f mednaffe.exe ../../../mednaffe.exe
 endef
