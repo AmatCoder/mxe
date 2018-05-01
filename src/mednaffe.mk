3410a17e76efc99d34c3cb8ed9af28b9b8bf526d
AmatCoder <amatcoder@gmail.com>
2018-05-01 15:53:15 +0200

    Add some commands (upx & zip) for releases

--- src/mednaffe.mk
+++ src/mednaffe.mk
@@ -18,4 +18,8 @@ endef
 define $(PKG)_BUILD
     cd '$(1)'/share/win && $(TARGET)-windres mednaffe.rc -O coff -o mednaffe.res && mv -f *.txt -t ../../../../
     cd '$(1)'/src && $(TARGET)-gcc -Wl,--export-all-symbols -O2 -s -std=c99 -Wall -DGTK2_ENABLED -DSTATIC_ENABLED -mwindows -mms-bitfields -o mednaffe.exe mednaffe.c active.c command.c gui.c list.c toggles.c prefs.c about.c input.c log.c  joystick_win.c md5.c resource.c ../share/win/mednaffe.res `i686-w64-mingw32.static-pkg-config --libs --cflags sdl2 gtk+-2.0 gmodule-export-2.0` && mv -f mednaffe.exe ../../../mednaffe.exe
+
+    cd '$(1)'/../../  && $(TARGET)-strip -s mednaffe.exe
+    # cd '$(1)'/../../  && upx --best --compress-icons=0 mednaffe.exe
+    # cd '$(1)'/../../ && zip -r - *.txt mednaffe-licenses mednaffe.exe > mednaffe-$($(PKG)_VERSION)-win.zip
 endef
