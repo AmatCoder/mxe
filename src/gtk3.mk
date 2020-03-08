69be89e94f7df5ee0a5ca64497d62636d2e25b0d
AmatCoder <amatcoder@gmail.com>
2020-03-08 21:43:44 +0100

    Update gtk3 to 3.22.30
    Add a Windows10 theme
    Remove icons

--- src/gtk3.mk
+++ src/gtk3.mk
@@ -4,31 +4,47 @@ PKG             := gtk3
 $(PKG)_WEBSITE  := https://gtk.org/
 $(PKG)_DESCR    := GTK+
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.22.7
-$(PKG)_CHECKSUM := a3a27564bfb1679ebbc75c37cd2bcd6e727c8bdfbcd3984d29305bf9ee60d432
+$(PKG)_VERSION  := 3.22.30
+$(PKG)_CHECKSUM := a1a4a5c12703d4e1ccda28333b87ff462741dc365131fbc94c218ae81d9a6567
 $(PKG)_SUBDIR   := gtk+-$($(PKG)_VERSION)
 $(PKG)_FILE     := gtk+-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := https://download.gnome.org/sources/gtk+/$(call SHORT_PKG_VERSION,$(PKG))/$($(PKG)_FILE)
-$(PKG)_DEPS     := cc atk cairo gdk-pixbuf gettext glib jasper jpeg libepoxy libpng pango tiff
+$(PKG)_DEPS     := cc atk cairo gdk-pixbuf glib jasper jpeg libepoxy libpng pango tiff
 
 define $(PKG)_UPDATE
-    $(WGET) -q -O- 'https://gitlab.gnome.org/GNOME/gtk+/tags' | \
-    $(SED) -n "s,.*<a [^>]\+>v\?\([0-9]\+\.[0-9.]\+\)<.*,\1,p" | \
+    $(WGET) -q -O- 'https://git.gnome.org/browse/gtk+/refs/tags' | \
+    grep '<a href=' | \
+    $(SED) -n 's,.*<a[^>]*>\([0-9]*\.[0-9]*[02468]\.[^<]*\)<.*,\1,p' | \
     grep '^3\.' | \
-    grep -v '^3\.9[0-9]' | \
     head -1
 endef
 
 define $(PKG)_BUILD
+    cp ./src/gtk3/*.png '$(SOURCE_DIR)'/gtk/icons/16x16/actions/
+    cp ./src/gtk3/*.png '$(SOURCE_DIR)'/gtk/icons/16x16/status/
+    mkdir -p '$(SOURCE_DIR)'/gtk/theme/Windows10/assets/
+    cp ./src/gtk3/Windows10/*.* '$(SOURCE_DIR)'/gtk/theme/Windows10/
+    cp ./src/gtk3/Windows10/assets/*.* '$(SOURCE_DIR)'/gtk/theme/Windows10/assets/
     cd '$(BUILD_DIR)' && '$(SOURCE_DIR)/configure' \
         $(MXE_CONFIGURE_OPTS) \
+        --enable-explicit-deps \
         --disable-glibtest \
+        --disable-modules \
+        --disable-xinput \
+        --disable-packagekit \
         --disable-cups \
+        --disable-papi \
+        --disable-cloudprint \
         --disable-test-print-backend \
         --disable-gtk-doc \
         --disable-man \
+        --disable-nls \
+        --enable-debug=yes \
+        --enable-introspection=no \
+        --enable-colord=no \
         --with-included-immodules \
-        --enable-win32-backend
+        --enable-win32-backend \
+        --without-x
     $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)' $(MXE_DISABLE_CRUFT) EXTRA_DIST=
     $(MAKE) -C '$(BUILD_DIR)' -j 1 install $(MXE_DISABLE_CRUFT) EXTRA_DIST=
 
